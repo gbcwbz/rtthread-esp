4b2e56056f69eedd96c5a8767a57db6a136a1912
bernard <bernard.xiong@rt-thread.com>
2017-06-15 09:39:56 +0800

    Remove component.mk file.

--- rt-thread/component.mk
+++
@@ -1,7 +0,0 @@
-#
-# Component Makefile
-#
-
-COMPONENT_ADD_INCLUDEDIRS := . include components/drivers/include components/finsh bsp
-COMPONENT_PRIV_INCLUDEDIRS := . include components/drivers/include components/finsh bsp
-COMPONENT_SRCDIRS := src bsp components/drivers/src components/drivers/serial components/finsh
