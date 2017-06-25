3589d80f06b34cd95d12ec23d1f490846229e8eb
Bernard Xiong <bernard.xiong@gmail.com>
2017-06-25 12:06:30 +0800

    Add more code/Kconfig for menuconfig.

--- rtconfig.h
+++ rtconfig.h
@@ -1,170 +1,175 @@
-/* RT-Thread config file */
-#ifndef __RTTHREAD_CFG_H__
-#define __RTTHREAD_CFG_H__
+#ifndef RT_CONFIG_H__
+#define RT_CONFIG_H__
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/timers.h"
-#include "freertos/queue.h"
-#include "freertos/StackMacros.h"
-#include "freertos/xtensa_rtos.h"
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread Project Configuration */
 
-/* RT_NAME_MAX*/
-#define RT_NAME_MAX	16
+/* RT-Thread Kernel */
 
-/* RT_ALIGN_SIZE*/
-#define RT_ALIGN_SIZE	4
-
-/* PRIORITY_MAX */
-#define RT_THREAD_PRIORITY_MAX	32
-
-/* Tick per Second */
-#define RT_TICK_PER_SECOND	CONFIG_FREERTOS_HZ
-
-/* SECTION: RT_DEBUG */
-/* Thread Debug */
+#define RT_NAME_MAX 8
+#define RT_ALIGN_SIZE 4
+#define RT_THREAD_PRIORITY_MAX 32
+#define RT_TICK_PER_SECOND 1000
 #define RT_DEBUG
-
-/* Using Hook */
+#define RT_USING_OVERFLOW_CHECK
+/* CONFIG_RT_DEBUG_INIT is not set */
+/* CONFIG_RT_DEBUG_THREAD is not set */
 #define RT_USING_HOOK
-
-#define IDLE_THREAD_STACK_SIZE 2048
-//#define RT_USING_OVERFLOW_CHECK
-
-/* Using Software Timer */
+#define RT_USING_IDLE_HOOK
 #define RT_USING_TIMER_SOFT
-#define RT_TIMER_THREAD_PRIO		12
-#define RT_TIMER_THREAD_STACK_SIZE	2048
+#define RT_TIMER_THREAD_PRIO 4
+#define RT_TIMER_THREAD_STACK_SIZE 2048
+#define IDLE_THREAD_STACK_SIZE 1024
 
-/* SECTION: IPC */
-/* Using Semaphore*/
+/* Inter-Thread communication */
+
 #define RT_USING_SEMAPHORE
-
-/* Using Mutex */
 #define RT_USING_MUTEX
-
-/* Using Event */
 #define RT_USING_EVENT
-
-/* Using MailBox */
 #define RT_USING_MAILBOX
-
-/* Using Message Queue */
 #define RT_USING_MESSAGEQUEUE
 
-/* Using Dynamic Heap Management */
-#define RT_USING_HEAP
-#define RT_USING_MEMHEAP
+/* Memory Management */
+
 #define RT_USING_MEMPOOL
+#define RT_USING_MEMHEAP
+#define RT_USING_HEAP
+#define RT_USING_SMALL_MEM
+/* CONFIG_RT_USING_SLAB is not set */
 
-/* SECTION: Device System */
-/* Using Device System */
+/* Kernel Device Object */
+
 #define RT_USING_DEVICE
-#define RT_USING_DEVICE_IPC
-#define RT_USING_SERIAL
-#define RT_USING_SPI
-#define RT_USING_I2C
-#define RT_USING_I2C_BITOPS
-#define RT_USING_PIN
-
-/* SECTION: Console options */
 #define RT_USING_CONSOLE
-/* the buffer size of console*/
-#define RT_CONSOLEBUF_SIZE	128
+#define RT_CONSOLEBUF_SIZE 128
+#define RT_CONSOLE_DEVICE_NAME "uart"
+/* CONFIG_RT_USING_MODULE is not set */
 
-/* SECTION: finsh, a C-Express shell */
+/* RT-Thread Components */
+
+/* CONFIG_RT_USING_COMPONENTS_INIT is not set */
+
+/* C++ features */
+
+/* CONFIG_RT_USING_CPLUSPLUS is not set */
+
+/* Command shell */
+
 #define RT_USING_FINSH
+#define FINSH_USING_SYMTAB
+#define FINSH_USING_DESCRIPTION
+#define FINSH_THREAD_STACK_SIZE 4096
+/* CONFIG_FINSH_USING_AUTH is not set */
+#define FINSH_DEFAULT_PASSWORD "rtthread"
 #define FINSH_USING_MSH
 #define FINSH_USING_MSH_DEFAULT
 #define FINSH_USING_MSH_ONLY
 
-/* Using symbol table */
-#define FINSH_USING_SYMTAB
-#define FINSH_USING_DESCRIPTION
-#define FINSH_THREAD_STACK_SIZE		4096
+/* Device virtual file system */
 
-/* SECTION: a runtime libc library */
-/* a runtime libc library */
+#define RT_USING_DFS
+#define DFS_USING_WORKDIR
+#define DFS_FILESYSTEMS_MAX 2
+#define DFS_FD_MAX 4
+#define RT_USING_DFS_ELMFAT
+#define RT_DFS_ELM_CODE_PAGE 437
+#define RT_DFS_ELM_MAX_LFN 256
+#define RT_DFS_ELM_DRIVES 2
+#define RT_DFS_ELM_MAX_SECTOR_SIZE 512
+/* CONFIG_RT_DFS_ELM_USE_ERASE is not set */
+#define RT_DFS_ELM_REENTRANT
+#define RT_USING_DFS_DEVFS
+/* CONFIG_RT_USING_DFS_NET is not set */
+/* CONFIG_RT_USING_DFS_NFS is not set */
+
+/* Device Drivers */
+
+#define RT_USING_DEVICE_IPC
+#define RT_USING_SERIAL
+/* CONFIG_RT_USING_CAN is not set */
+/* CONFIG_RT_USING_HWTIMER is not set */
+#define RT_USING_I2C
+#define RT_USING_I2C_BITOPS
+#define RT_USING_PIN
+/* CONFIG_RT_USING_MTD_NOR is not set */
+/* CONFIG_RT_USING_MTD_NAND is not set */
+/* CONFIG_RT_USING_RTC is not set */
+/* CONFIG_RT_USING_SDIO is not set */
+/* CONFIG_RT_USING_SPI is not set */
+/* CONFIG_RT_USING_WDT is not set */
+/* CONFIG_RT_USING_USB_HOST is not set */
+/* CONFIG_RT_USING_USB_DEVICE is not set */
+
+/* libc */
 
 #define RT_USING_LIBC
 #define RT_USING_PTHREADS
 
-// <section name="RT_USING_DFS" description="Device file system" default="true" >
-#define RT_USING_DFS
-// <bool name="DFS_USING_WORKDIR" description="Using working directory" default="true" />
-#define DFS_USING_WORKDIR
-// <integer name="DFS_FILESYSTEMS_MAX" description="The maximal number of mounted file system" default="4" />
-#define DFS_FILESYSTEMS_MAX	4
-// <integer name="DFS_FD_MAX" description="The maximal number of opened files" default="4" />
-#define DFS_FD_MAX	16
-// <bool name="RT_USING_DFS_ELMFAT" description="Using ELM FatFs" default="true" />
-#define RT_USING_DFS_ELMFAT
-#define RT_DFS_ELM_DRIVES           2
-#define RT_DFS_ELM_REENTRANT
-#define RT_DFS_ELM_MAX_SECTOR_SIZE  4096
+/* Network stack */
 
-// <integer name="RT_DFS_ELM_USE_LFN" description="Support long file name" default="0">
-// <item description="LFN with static LFN working buffer">1</item>
-// <item description="LFN with dynamic LFN working buffer on the stack">2</item>
-// <item description="LFN with dynamic LFN working buffer on the heap">3</item>
-// </integer>
-#define RT_DFS_ELM_USE_LFN	3
-// <integer name="RT_DFS_ELM_CODE_PAGE" description="OEM code page" default="437">
-#define RT_DFS_ELM_CODE_PAGE	437
-// <integer name="RT_DFS_ELM_MAX_LFN" description="Maximal size of file name length" default="128" />
-#define RT_DFS_ELM_MAX_LFN	128
-// <bool name="RT_USING_DFS_YAFFS2" description="Using YAFFS2" default="false" />
-// #define RT_USING_DFS_YAFFS2
-// <bool name="RT_USING_DFS_UFFS" description="Using UFFS" default="false" />
-// #define RT_USING_DFS_UFFS
-// <bool name="RT_USING_DFS_DEVFS" description="Using devfs for device objects" default="true" />
-#define RT_USING_DFS_DEVFS
-// </section>
+/* light weight TCP/IP stack */
 
-// <section name="RT_USING_LWIP" description="lwip, a lightweight TCP/IP protocol stack" default="true" >
 #define RT_USING_LWIP
-// <bool name="RT_LWIP_ICMP" description="Enable ICMP protocol" default="true" />
+/* CONFIG_RT_USING_LWIP141 is not set */
+/* CONFIG_RT_USING_LWIP200 is not set */
+#define RT_USING_LWIP202
+#define RT_LWIP_IGMP
 #define RT_LWIP_ICMP
-// <bool name="RT_LWIP_IGMP" description="Enable IGMP protocol" default="false" />
-// #define RT_LWIP_IGMP
-// <bool name="RT_LWIP_UDP" description="Enable UDP protocol" default="true" />
-#define RT_LWIP_UDP
-// <bool name="RT_LWIP_TCP" description="Enable TCP protocol" default="true" />
-#define RT_LWIP_TCP
-// <bool name="RT_LWIP_DNS" description="Enable DNS protocol" default="true" />
+/* CONFIG_RT_LWIP_SNMP is not set */
 #define RT_LWIP_DNS
-// <bool name="RT_LWIP_SNMP" description="Enable SNMP protocol" default="false" />
-// #define RT_LWIP_SNMP
-// <bool name="RT_LWIP_DHCP" description="Enable DHCP client to get IP address" default="false" />
 #define RT_LWIP_DHCP
-// <integer name="RT_LWIP_TCPTHREAD_PRIORITY" description="the thread priority of TCP thread" default="128" />
-#define RT_LWIP_TCPTHREAD_PRIORITY	12
-// <integer name="RT_LWIP_TCPTHREAD_MBOX_SIZE" description="the mail box size of TCP thread to wait for" default="32" />
-#define RT_LWIP_TCPTHREAD_MBOX_SIZE	8
-// <integer name="RT_LWIP_TCPTHREAD_STACKSIZE" description="the thread stack size of TCP thread" default="4096" />
-#define RT_LWIP_TCPTHREAD_STACKSIZE	4096
-// <integer name="RT_LWIP_ETHTHREAD_PRIORITY" description="the thread priority of ethnetif thread" default="144" />
-#define RT_LWIP_ETHTHREAD_PRIORITY	14
-// <integer name="RT_LWIP_ETHTHREAD_MBOX_SIZE" description="the mail box size of ethnetif thread to wait for" default="8" />
-#define RT_LWIP_ETHTHREAD_MBOX_SIZE	8
-// <integer name="RT_LWIP_ETHTHREAD_STACKSIZE" description="the stack size of ethnetif thread" default="512" />
-#define RT_LWIP_ETHTHREAD_STACKSIZE	512
-// <ipaddr name="RT_LWIP_IPADDR" description="IP address of device" default="192.168.1.30" />
-#define RT_LWIP_IPADDR0 192
-#define RT_LWIP_IPADDR1 168
-#define RT_LWIP_IPADDR2 1
-#define RT_LWIP_IPADDR3 30
-// <ipaddr name="RT_LWIP_GWADDR" description="Gateway address of device" default="192.168.1.1" />
-#define RT_LWIP_GWADDR0 192
-#define RT_LWIP_GWADDR1 168
-#define RT_LWIP_GWADDR2 1
-#define RT_LWIP_GWADDR3 1
-// <ipaddr name="RT_LWIP_MSKADDR" description="Mask address of device" default="255.255.255.0" />
-#define RT_LWIP_MSKADDR0 255
-#define RT_LWIP_MSKADDR1 255
-#define RT_LWIP_MSKADDR2 255
-#define RT_LWIP_MSKADDR3 0
-// </section>
+#define IP_SOF_BROADCAST 1
+#define IP_SOF_BROADCAST_RECV 1
+#define LWIP_USING_DHCPD
+#define RT_LWIP_UDP
+#define RT_LWIP_TCP
+/* CONFIG_RT_LWIP_PPP is not set */
+#define RT_LWIP_PBUF_NUM 16
+#define RT_LWIP_RAW_PCB_NUM 4
+#define RT_LWIP_UDP_PCB_NUM 4
+#define RT_LWIP_TCP_PCB_NUM 4
+#define RT_LWIP_TCP_SEG_NUM 40
+#define RT_LWIP_TCP_SND_BUF 8196
+#define RT_LWIP_TCP_WND 8196
+#define RT_LWIP_TCPTHREAD_PRIORITY 10
+#define RT_LWIP_TCPTHREAD_MBOX_SIZE 8
+#define RT_LWIP_TCPTHREAD_STACKSIZE 1024
+#define RT_LWIP_ETHTHREAD_PRIORITY 12
+#define RT_LWIP_ETHTHREAD_STACKSIZE 1024
+#define RT_LWIP_ETHTHREAD_MBOX_SIZE 8
+#define RT_LWIP_REASSEMBLY_FRAG
+#define LWIP_NETIF_STATUS_CALLBACK 1
+/* CONFIG_LWIP_SOCKOPT is not set */
+
+/* Modbus master and slave stack */
+
+/* CONFIG_RT_USING_MODBUS is not set */
+
+/* Board Support Package */
+
+#define RT_USING_UART1
+#define RT_USING_UART2
+#define CONFIG_CHIP_ESP32
+#define CONFIG_BOARD_ESP32
+/* CONFIG_CONFIG_BOARD_ESP32_RTAUDIO is not set */
+#define RT_USING_WIFI
+
+/* RT-Thread online packages */
+
+/* IoT - internet of things */
+
+/* language packages */
+
+/* miscellaneous packages */
+
+/* CONFIG_PKG_USING_HELLO is not set */
+
+/* multimedia packages */
+
+/* security packages */
+
+/* system packages */
+
+#include "rtconfig_project.h"
 
 #endif
-
